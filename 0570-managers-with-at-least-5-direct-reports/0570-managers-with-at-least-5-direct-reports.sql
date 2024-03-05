8c250c5337e4fc61f57c2c0d95e100ab90e6dd2b
striver11 <83543044+striver11@users.noreply.github.com>
2024-03-05 16:36:50 +0530

    Time: 781 ms (82.12%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,5 @@
-
-select name 
-from Employee 
-where id in(
-SELECT managerId
-FROM Employee
-GROUP BY managerId
-HAVING COUNT(*) >= 5);
+SELECT e1.name
+FROM Employee e1
+JOIN Employee e2 ON e1.id = e2.managerId
+GROUP BY e1.name,e2.managerId
+HAVING COUNT(*) >= 5;
